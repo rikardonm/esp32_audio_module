25bb286c62bb5d82e58b228f1b902b6eb2fa5183
ricardo <ricardo@azevem.com>
2019-07-01 23:11:22 +0200

    created and configured eclipse project

--- src/sinker/main/main.cpp
+++ src/sinker/main/main.cpp
@@ -1,16 +1,3 @@
-// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 extern "C" {
 
@@ -35,13 +22,21 @@ extern "C" {
 	#include "esp_avrc_api.h"
 	#include "driver/i2s.h"
 
-
-	/* event for handler "bt_av_hdl_stack_up */
-	enum {
-		BT_APP_EVT_STACK_UP = 0,
-	};
-
+	// export symbols expected by C code
+	extern void app_main();
 };
+
+#include <string>
+#include <cctype>
+
+
+
+/* event for handler "bt_av_hdl_stack_up */
+enum {
+	BT_APP_EVT_STACK_UP = 0,
+};
+
+
 
 /* handler for bluetooth stack enabled events */
 static void bt_av_hdl_stack_evt(uint16_t event, void *p_param);
@@ -92,15 +87,103 @@ static void bt_av_hdl_stack_evt(uint16_t event, void *p_param);
  *
  *
  *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 
-/* what var type is esp_bt_pin_type_t, is it char? */
-struct SinkerOptions {
-		char pin_code*;
-		char device_name*;
-		bool enabled;
-		float output_volume;
+
+class BluetoothInterface {
+public:
+	bool ValidadePinCode(std::string npin) {
+		// check pin pre-requisites
+		// 1 - length: 4-16 chars, non-null terminated
+		// 2 - char types: 0-9
+		if (npin.length() > 16 || npin.length() < 4)
+			return false;
+		for (auto& ch : npin) {
+			if (!std::isdigit(ch)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool SetNewPinCode(std::string npin) {
+		if (ValidadePinCode(npin)) {
+			esp_bt_pin_type_t pin_type = ESP_BT_PIN_TYPE_FIXED;
+//			esp_bt_gap_set_pin(pin_type, npin.length(), static_cast<esp_bt_pin_code_t>(npin.c_str()));
+			return true;
+		}
+		return false;
+	}
+
+	void Boot()
+	{
+		esp_err_t err;
+	    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BLE));
+	    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+	    if ((err = esp_bt_controller_init(&bt_cfg)) != ESP_OK) {
+	        ESP_LOGE(BT_AV_TAG, "%s initialize controller failed: %s\n", __func__, esp_err_to_name(err));
+	        return;
+	    }
+	    if ((err = esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT)) != ESP_OK) {
+	        ESP_LOGE(BT_AV_TAG, "%s enable controller failed: %s\n", __func__, esp_err_to_name(err));
+	        return;
+	    }
+	    if ((err = esp_bluedroid_init()) != ESP_OK) {
+	        ESP_LOGE(BT_AV_TAG, "%s initialize bluedroid failed: %s\n", __func__, esp_err_to_name(err));
+	        return;
+	    }
+	    if ((err = esp_bluedroid_enable()) != ESP_OK) {
+	        ESP_LOGE(BT_AV_TAG, "%s enable bluedroid failed: %s\n", __func__, esp_err_to_name(err));
+	        return;
+	    }
+	    /* create application task */
+	    bt_app_task_start_up();
+	    /* Bluetooth device name, connection mode and profile set up */
+	    bt_app_work_dispatch(bt_av_hdl_stack_evt, BT_APP_EVT_STACK_UP, NULL, 0, NULL);
+	    // SSP
+	    /* Set default parameters for Secure Simple Pairing */
+	    esp_bt_sp_param_t param_type = ESP_BT_SP_IOCAP_MODE;
+	    esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_IO;
+	    esp_bt_gap_set_security_param(param_type, &iocap, sizeof(uint8_t));
+	    SetNewPinCode("FUCK;");
+	}
 };
+
+
+/* what var type is esp_bt_pin_type_t, is it char? */
+struct Sinker {
+public:
+	Sinker(std::string name, std::string pin, float volume) :
+			device_name(name), pin_code(pin), output_volume(volume) {
+	}
+	BluetoothInterface bti;
+	std::string device_name;
+	std::string pin_code;
+	bool enabled = false;
+	float output_volume;
+
+	void Boot()
+	{
+		bti.Boot();
+	}
+};
+
+
+
+
+
+
+Sinker sys("mbtt-0", "7864", 0);
+
+
+
+
 
 
 void app_main()
@@ -114,77 +197,32 @@ void app_main()
     ESP_ERROR_CHECK(err);
 
     i2s_config_t i2s_config = {
-        .mode = I2S_MODE_MASTER | I2S_MODE_TX,                                  // Only TX
+        .mode = static_cast<i2s_mode_t>(I2S_MODE_MASTER | I2S_MODE_TX),                                  // Only TX
         .sample_rate = 44100,
-        .bits_per_sample = 16,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,                           //2-channels
         .communication_format = I2S_COMM_FORMAT_I2S_MSB,
+        .intr_alloc_flags = 0,                                                  //Default interrupt priority
         .dma_buf_count = 6,
         .dma_buf_len = 60,
-        .intr_alloc_flags = 0,                                                  //Default interrupt priority
-        .tx_desc_auto_clear = true                                              //Auto clear tx descriptor on underflow
+		.use_apll = false,
+        .tx_desc_auto_clear = true,                                              //Auto clear tx descriptor on underflow
+		.fixed_mclk = 0
     };
 
+    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, nullptr);
 
-    i2s_driver_install(0, &i2s_config, 0, NULL);
     i2s_pin_config_t pin_config = {
-        .bck_io_num = CONFIG_EXAMPLE_I2S_BCK_PIN,
-        .ws_io_num = CONFIG_EXAMPLE_I2S_LRCK_PIN,
-        .data_out_num = CONFIG_EXAMPLE_I2S_DATA_PIN,
+        .bck_io_num = CONFIG_MYBTT_ESP32_AUDIO_MODULE_I2S_BLK_PIN,
+        .ws_io_num = CONFIG_MYBTT_ESP32_AUDIO_MODULE_I2S_LCK_PIN,
+        .data_out_num = CONFIG_MYBTT_ESP32_AUDIO_MODULE_I2S_DIN_PIN,
         .data_in_num = -1                                                       //Not used
     };
 
-    i2s_set_pin(0, &pin_config);
+    i2s_set_pin(I2S_NUM_0, &pin_config);
 
-
-    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BLE));
-
-    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-    if ((err = esp_bt_controller_init(&bt_cfg)) != ESP_OK) {
-        ESP_LOGE(BT_AV_TAG, "%s initialize controller failed: %s\n", __func__, esp_err_to_name(err));
-        return;
-    }
-
-    if ((err = esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT)) != ESP_OK) {
-        ESP_LOGE(BT_AV_TAG, "%s enable controller failed: %s\n", __func__, esp_err_to_name(err));
-        return;
-    }
-
-    if ((err = esp_bluedroid_init()) != ESP_OK) {
-        ESP_LOGE(BT_AV_TAG, "%s initialize bluedroid failed: %s\n", __func__, esp_err_to_name(err));
-        return;
-    }
-
-    if ((err = esp_bluedroid_enable()) != ESP_OK) {
-        ESP_LOGE(BT_AV_TAG, "%s enable bluedroid failed: %s\n", __func__, esp_err_to_name(err));
-        return;
-    }
-
-    /* create application task */
-    bt_app_task_start_up();
-
-    /* Bluetooth device name, connection mode and profile set up */
-    bt_app_work_dispatch(bt_av_hdl_stack_evt, BT_APP_EVT_STACK_UP, NULL, 0, NULL);
-
-#if (CONFIG_BT_SSP_ENABLED == true)
-    /* Set default parameters for Secure Simple Pairing */
-    esp_bt_sp_param_t param_type = ESP_BT_SP_IOCAP_MODE;
-    esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_IO;
-    esp_bt_gap_set_security_param(param_type, &iocap, sizeof(uint8_t));
-#endif
-
-    /*
-     * Set default parameters for Legacy Pairing
-     * Use fixed pin code
-     */
-    esp_bt_pin_type_t pin_type = ESP_BT_PIN_TYPE_FIXED;
-    esp_bt_pin_code_t pin_code;
-    pin_code[0] = '1';
-    pin_code[1] = '2';
-    pin_code[2] = '3';
-    pin_code[3] = '4';
-    esp_bt_gap_set_pin(pin_type, 4, pin_code);
-
+    //bluetooth setup
+    sys.Boot();
 }
 
 void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
@@ -200,7 +238,8 @@ void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
         break;
     }
 
-#if (CONFIG_BT_SSP_ENABLED == true)
+
+    // SSP
     case ESP_BT_GAP_CFM_REQ_EVT:
         ESP_LOGI(BT_AV_TAG, "ESP_BT_GAP_CFM_REQ_EVT Please compare the numeric value: %d", param->cfm_req.num_val);
         esp_bt_gap_ssp_confirm_reply(param->cfm_req.bda, true);
@@ -211,8 +250,6 @@ void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
     case ESP_BT_GAP_KEY_REQ_EVT:
         ESP_LOGI(BT_AV_TAG, "ESP_BT_GAP_KEY_REQ_EVT Please enter passkey!");
         break;
-#endif
-
     default: {
         ESP_LOGI(BT_AV_TAG, "event: %d", event);
         break;
@@ -227,8 +264,11 @@ static void bt_av_hdl_stack_evt(uint16_t event, void *p_param)
     switch (event) {
     case BT_APP_EVT_STACK_UP: {
         /* set up device name */
-        char *dev_name = "ESP_SPEAKER";
-        esp_bt_dev_set_device_name(dev_name);
+
+    	// todo name here
+    	const char* dev_name = "FUCk";
+
+    	esp_bt_dev_set_device_name(dev_name);
 
         esp_bt_gap_register_callback(bt_app_gap_cb);
 
